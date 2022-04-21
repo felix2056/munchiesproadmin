86b672dfad72dc84bcf78b3db4359869c3c9eb4d
Daniel Felix <felixdaniel2056@gmail.com>
2022-04-21 21:55:34 +0100

    chore: minor changes 3

--- pages/products/add.php
+++ pages/products/add.php
@@ -46,7 +46,7 @@
         }
 
         // Allow certain file formats
-        if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif" ) {
+        if($imageFileType != "jpg" || $imageFileType != "png" || $imageFileType != "jpeg" || $imageFileType != "gif" || $imageFileType != "webp" ) {
           echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
           $uploadOk = 0;
         }
