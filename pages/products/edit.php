558abae293bbcb911ad028caf899484bfec814f1
Daniel Felix <felixdaniel2056@gmail.com>
2022-04-21 22:35:50 +0100

    chore: minor changes 5

--- pages/products/edit.php
+++ pages/products/edit.php
@@ -40,10 +40,10 @@
         }
 
         // Allow certain file formats
-        if($imageFileType != "jpg" || $imageFileType != "png" || $imageFileType != "jpeg" || $imageFileType != "gif" || $imageFileType != "webp" ) {
-          $error_msg .= "Sorry, only JPG, JPEG, PNG, GIF & WEBP files are allowed. <br>";
-          $uploadOk = 0;
-        }
+        // if($imageFileType != "jpg" || $imageFileType != "png" || $imageFileType != "jpeg" || $imageFileType != "gif" || $imageFileType != "webp" ) {
+        //   $error_msg .= "Sorry, only JPG, JPEG, PNG, GIF & WEBP files are allowed. <br>";
+        //   $uploadOk = 0;
+        // }
 
         // Check if $uploadOk is set to 0 by an error
         if ($uploadOk == 0) {
